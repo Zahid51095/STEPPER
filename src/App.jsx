d3cc960b7f9901f3d17a5317cbbb429c5ef6ca57
Zahid Hasan <zahidhasan140338@gmail.com>
2024-02-16 19:42:53 +0600

    completed the stepper app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
 
 import './App.css'
+import CheckoutStepper from './component/stepper';
+
+const CHECKOUT_STEPS = [
+  {
+    name: "Customer Info",
+    Component: () => <div>Provide your contact details.</div>,
+  },
+  {
+    name: "Shipping Info",
+    Component: () => <div>Enter your shipping address.</div>,
+  },
+  {
+    name: "Payment",
+    Component: () => <div>Complete payment for your order.</div>,
+  },
+  {
+    name: "Delivered",
+    Component: () => <div> Your order has been delivered.</div>,
+  },
+];
 
 function App() {
  
 
   return (
-   <div>Friday Fun Project</div>
+   <div>
+     <h2>Checkout</h2>
+      <CheckoutStepper stepsConfig={CHECKOUT_STEPS} />
+   </div>
   )
 }
 
